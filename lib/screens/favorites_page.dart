5dba762ffb219bdd26bab52c4feb7958ca173117
Abenezer Abebe <abebeabenezer808@gmail.com>
2025-04-16 01:33:50 -0700

    Update favorites_page.dart

--- lib/screens/favorites_page.dart
+++ lib/screens/favorites_page.dart
@@ -3,3 +3,53 @@ import 'package:flutter/material.dart';
 import '../models/property_model.dart';
 import '../services/supabase_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+class FavoritesPage extends StatefulWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  final SupabaseService _supabaseService = SupabaseService(
+    supabase: Supabase.instance.client,
+  );
+
+  List<PropertyModel> _favorites = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    setState(() => _isLoading = true);
+    try {
+      // Get favorite property IDs first
+      final favoriteIds = await _supabaseService.getUserFavorites();
+
+      // Then get all properties and filter for favorites
+      final allProperties = await _supabaseService.getProperties();
+      final favorites =
+          allProperties
+              .where((property) => favoriteIds.contains(property.id))
+              .toList();
+
+      if (mounted) {
+        setState(() {
+          _favorites = favorites;
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() => _isLoading = false);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error loading favorites: ${e.toString()}')),
+        );
+      }
+    }
+  }
