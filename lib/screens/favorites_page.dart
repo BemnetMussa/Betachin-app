bb9e611878545ad6184137f514ebee6a719771a0
Abenezer Abebe <abebeabenezer808@gmail.com>
2025-04-16 01:32:53 -0700

    Update favorites_page.dart

---
+++ lib/screens/favorites_page.dart
@@ -0,0 +1,5 @@
+// lib/screens/favorites_page.dart
+import 'package:flutter/material.dart';
+import '../models/property_model.dart';
+import '../services/supabase_service.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
