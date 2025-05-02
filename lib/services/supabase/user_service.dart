fcfd1f6423468ab5ce17861e0a13202f339b6ccb
BemnetMussa <bemnetmussa@gmail.com>
2025-05-02 11:31:59 +0300

    Add: Implement UserService class with SupabaseClient integration

---
+++ lib/services/supabase/user_service.dart
@@ -0,0 +1,12 @@
+import 'package:logging/logging.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class UserService {
+  final SupabaseClient supabase;
+  final _logger = Logger('UserService');
+
+  UserService({required this.supabase});
+
+
+
+}
