d0762086447d328fac00b73439133b1fb89775db
BemnetMussa <bemnetmussa@gmail.com>
2025-05-02 11:11:03 +0300

    FIx: bug fix on supabase services

--- lib/services/no_web.dart
+++ lib/services/no_web.dart
@@ -6,9 +6,3 @@ class File {
   final String type = '';
 }
 
-// Empty class to satisfy FileReader references
-class FileReader {
-  dynamic result;
-  Stream<dynamic> get onLoadEnd => const Stream.empty();
-  void readAsArrayBuffer(dynamic file) {}
-}
